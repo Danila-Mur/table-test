f6822a1f1735f7d002005fd85aecf98ed7e4da2f
Danil Murzin <danil.murzin.200026@gmail.com>
2023-08-06 17:00:27 +0300

    fix build

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 
 import { App } from './App.jsx';
 import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
